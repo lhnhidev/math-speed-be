7e4274129f650dfdee3790da07004416f71930fe
lhnhidev <lhnhi420@gmail.com>
2025-08-24 06:23:50 +0700

    change api

--- public/Math_Speed_v1/api.js
+++ public/Math_Speed_v1/api.js
@@ -1,9 +1,10 @@
-export const GET_CAU_HOI = "http://localhost:3000/api/cau-hoi";
-export const LOGIN = "http://localhost:3000/api/users/login";
-export const REGISTER = "http://localhost:3000/api/users/";
-export const GET_USER = "http://localhost:3000/api/users/";
+export const GET_CAU_HOI = "https://math-speed-be.onrender.com/cau-hoi";
+export const LOGIN = "https://math-speed-be.onrender.com/users/login";
+export const REGISTER = "https://math-speed-be.onrender.com/users/";
+export const GET_USER = "https://math-speed-be.onrender.com/users/";
 export const ADD_TEST_ATTEMP =
-  "http://localhost:3000/api/lich-su-lam-bai/addForUser";
-export const GET_TEST_ATTEMP = "http://localhost:3000/api/lich-su-lam-bai/";
+  "https://math-speed-be.onrender.com/lich-su-lam-bai/addForUser";
+export const GET_TEST_ATTEMP =
+  "https://math-speed-be.onrender.com/lich-su-lam-bai/";
 export const GET_TEST_ATTEMP_BY_ID =
-  "http://localhost:3000/api/lich-su-lam-bai/getById/";
+  "https://math-speed-be.onrender.com/lich-su-lam-bai/getById/";
