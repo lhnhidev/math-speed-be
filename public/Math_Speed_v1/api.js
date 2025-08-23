75ca1a158876239823e1e7bcca76398874159d4e
lhnhidev <lhnhi420@gmail.com>
2025-08-24 06:30:05 +0700

    change api

--- public/Math_Speed_v1/api.js
+++ public/Math_Speed_v1/api.js
@@ -1,10 +1,10 @@
-export const GET_CAU_HOI = "https://math-speed-be.onrender.com/cau-hoi";
-export const LOGIN = "https://math-speed-be.onrender.com/users/login";
-export const REGISTER = "https://math-speed-be.onrender.com/users/";
-export const GET_USER = "https://math-speed-be.onrender.com/users/";
+export const GET_CAU_HOI = "https://math-speed-be.onrender.com/api/cau-hoi";
+export const LOGIN = "https://math-speed-be.onrender.com/api/users/login";
+export const REGISTER = "https://math-speed-be.onrender.com/api/users/";
+export const GET_USER = "https://math-speed-be.onrender.com/api/users/";
 export const ADD_TEST_ATTEMP =
-  "https://math-speed-be.onrender.com/lich-su-lam-bai/addForUser";
+  "https://math-speed-be.onrender.com/api/lich-su-lam-bai/addForUser";
 export const GET_TEST_ATTEMP =
-  "https://math-speed-be.onrender.com/lich-su-lam-bai/";
+  "https://math-speed-be.onrender.com/api/lich-su-lam-bai/";
 export const GET_TEST_ATTEMP_BY_ID =
-  "https://math-speed-be.onrender.com/lich-su-lam-bai/getById/";
+  "https://math-speed-be.onrender.com/api/lich-su-lam-bai/getById/";
