1f58d38861fdbd133abb519f535bb184708fa119
lhnhidev <lhnhi420@gmail.com>
2025-08-26 15:44:36 +0700

    .

--- index.ts
+++ index.ts
@@ -40,13 +40,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://lhnhidev.github.io",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
