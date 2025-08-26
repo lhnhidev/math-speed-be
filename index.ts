129c88dee5d8272ccb89350a878adb2513bb89af
lhnhidev <lhnhi420@gmail.com>
2025-08-26 16:09:58 +0700

    .

--- index.ts
+++ index.ts
@@ -41,7 +41,6 @@ const allowedOrigins = [
 app.use(
   cors({
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
